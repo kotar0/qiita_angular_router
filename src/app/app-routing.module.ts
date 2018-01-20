9e097c4379fecef99c803d219cc56c75477f98d4
kotar0 <koutaro.okuya@gmail.com>
2018-01-21 00:43:52 +0900

    ADD:hello.component追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HelloComponent } from './hello/hello.component'
 
 const routes: Routes = [
+    { path:'', component:HelloComponent},
     { path:'shape', loadChildren:'./shapes/shapes.module#ShapesModule' }
 ];
 
